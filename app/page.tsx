7e6ef3c03f15b6fbeb45f54994856325015c3b40
Aneeq-Tahir <aneeqtaslam391@gmail.com>
2023-06-08 13:22:56 +0500

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
-
+"use client";
+import { useEffect } from "react";
+import "./globals.css";
+// 8d084db
 export default function Home() {
+  const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=8d084db";
+  const searchMovies = async (title: string) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search[0].Title);
+  };
+  useEffect(() => {
+    searchMovies("uncharted");
+  }, []);
+
   return (
-    <></>
-  )
+    <>
+      <h1>App</h1>
+    </>
+  );
 }
